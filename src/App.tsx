9ece234830485ee59c3e762184bbfadb9d59fab4
Christian Sandoval <christiansandoval1018@outlook.com>
2023-06-17 02:08:06 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid">
+        <div className="box-green"></div>
+        <div className="box-blue"></div>
+        <div className="box-pink"></div>
+        <div className="box-orange"></div>
+        <div className="box-teal"></div>
+        <div className="box-grey"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
